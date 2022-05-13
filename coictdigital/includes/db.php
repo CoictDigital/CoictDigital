e528283fcde3fe79c60f768f27f786488a897d73
itsmushi <jmushi175@gmail.com>
2022-05-13 17:17:03 +0300

    merging

--- coictdigital/includes/db.php
+++ coictdigital/includes/db.php
@@ -1,14 +1,11 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "coict_digital";
 
-//real for deployment in heroku
-// $servername="us-cdbr-east-02.cleardb.com";
-// $username = "b9ac30daddae48";
-// $password = "e8625356";
-// $dbname="heroku_c8d476e1a5e6864";
+<?php
+require_once('credentials.php');
+
+$servername = $SERVERNAME;
+$username = $USERNAME;
+$password = $PASSWORD;
+$dbname = $DBNAME;
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
