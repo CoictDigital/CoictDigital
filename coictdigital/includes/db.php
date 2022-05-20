01f52a00f4e87b536770cdcf21cd8596f91052f6
Angela Jovin <67833415+AngelaJovin@users.noreply.github.com>
2022-05-20 23:41:43 +0300

    Add files via upload

---
+++ coictdigital/includes/db.php
@@ -0,0 +1,16 @@
+
+<?php
+require_once('credentials.php');
+
+$servername = $SERVERNAME;
+$username = $USERNAME;
+$password = $PASSWORD;
+$dbname = $DBNAME;
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
