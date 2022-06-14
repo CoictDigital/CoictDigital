c905f5f0091721261f74b28f5ea69ead1545051a
Angela Jovin <angelamwemezi@gmail.com>
2022-06-14 14:32:09 +0100

    logout and db

--- coictdigital/includes/leftNav.php
+++ coictdigital/includes/leftNav.php
@@ -40,6 +40,7 @@
                         echo "leacturerinvigilation.php";
                       }
                       ?>" class="nav-link scrollto"> <span>Exam Invigilation</span></a></li>
+        <li><a href="./../logout.php" class="nav-link scrollto"> <span>Logout</span></a></li>
       
       </ul>
     </nav><!-- .nav-menu -->
