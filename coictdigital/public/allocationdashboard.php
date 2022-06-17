f792bb3fffa22aa61d6ef35d0d97b60b0814f70d
jaymlaki34 <mlaki3434@gmail.com>
2022-06-17 14:42:12 +0300

    commit changes

--- coictdigital/public/allocationdashboard.php
+++ coictdigital/public/allocationdashboard.php
@@ -36,30 +36,68 @@ unset($_SESSION["studentFilledCount"]);
           <div class="section-title">
             <h2>MY ALLOCATION</h2>
           </div>
-          <div class="">
+          <!-- <div class="">
           <form>
               <H3>Hellow,</H3>
 
-              <B>Dr.Mlaki</B><?php echo $_SESSION['$userdata']; ?>
+              <B>Dr.Mlaki</B>
               <div><P> You have been allocated to  <h4>COURSE</h4></P>
                  <div> PRACTICAL ASSISTANT: </div>
                  <div> EVALUATOR: </div>
                  
                   <div>SEMESTER: </div> 
 
-
-                  
-                  <!-- <a href="http://" target="_blank" rel="noopener noreferrer">VIEW HISTORY</a> 
-              <a href="http://" target="_blank" rel="noopener noreferrer">OTHER ALLOCATION</a> -->
                 </div>
                 <button type="submit" formaction="http://localhost/coictdigital/coictdigital/public/previewhistory.php" class="allocationbutton">VIEW HISTORY</button>
                     <button type="submit" formaction="http://localhost/coictdigital/coictdigital/public/lecturerallocation.php" class="allocationbutton">OTHER ALLOCATION</button>
           </form>
-        </div>
+        </div> -->
 
 
-        
         </div>
+        <div class="p-4">
+      <div class="row">
+        <h4 class="">Staff Allocation Details</h4>
+        </div> 
+        <div class="card">
+                    
+      <div class="card-body">        
+
+        <div class="row">
+
+        <div class="col-sm-4">
+        <div class="all-profile">
+        <img src="../assets/img/profile.png" alt="">
+        </div>
+        </div>
+
+        <div class="col-sm-8"> 
+        <form>
+              <H3>Hellow,</H3>
+
+              <B>Dr.Mlaki</B><?php echo $row['instructor']; ?>  
+              <div><> You have been allocated to  
+                 <div><h4>Course:<?php echo $row['course_name']; ?></h4></div>
+                 <div> Practical/Tutorial Assistant: </div>
+                 <div> Evaluator:<?php echo $row['evaluator']; ?> </div>
+                 
+                  <div>Semester:  <?php echo $row['semester']; ?></div> 
+
+                </div>
+                <button type="submit" formaction="http://localhost/coictdigital/coictdigital/public/previewhistory.php" class="allocationbutton">VIEW HISTORY</button>
+                <button type="submit" formaction="http://localhost/coictdigital/coictdigital/public/lecturerallocation.php" class="allocationbutton">OTHER ALLOCATION</button>
+          </form>
+        <!-- <p>Instructor: <?php echo $row['instructor']; ?>  </p> -->
+        <!-- <p>Course Name:  <?php echo $row['course_name']; ?></p>
+        <p>Evaluator:  <?php echo $row['evaluator']; ?></p>
+        <p>Practical Assistant:  <?php echo $row['assistant']; ?></p>
+        <p>Semester:  <?php echo $row['semester']; ?></p>
+        </div> -->
+        </div>
+
+            </div>        
+        </div>
+    </div>
       </section><!-- End Form Section -->
 
       <!-- end of fading edit form-->
