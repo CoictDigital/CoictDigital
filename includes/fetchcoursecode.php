dc6b555af2ddf9244aa685b625a510a3cd7e7d2a
tayanah <christinamahenge9@gmail.com>
2022-06-28 23:43:12 +0300

    course filtering implemented

--- includes/fetchcoursecode.php
+++ includes/fetchcoursecode.php
@@ -1,16 +1,16 @@
 <?php 
 
-//$data['year'] = $_POST['Study_year'];
-//$data['sem'] = $_POST['semester'];
-
-//echo json_encode($data);
-// exit;
-
+if(isset($_POST['semester'])){
+  $data['year'] = $_POST['study_year'];
+$data['sem'] = $_POST['semester'];
 
 
     require_once("db.php");
-    $query ="SELECT course_code FROM courses";
+    $query ="SELECT course_code FROM courses WHERE semester='$data[sem]' and study_year='$data[year]'";
     $result = $conn->query($query);
     if($result->num_rows> 0){
       $options= mysqli_fetch_all($result, MYSQLI_ASSOC);
-    }?>
+      echo json_encode($options);
+    }
+  };
+?>
